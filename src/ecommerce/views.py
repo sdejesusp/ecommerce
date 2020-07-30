6a97d29b47ccb2515465819ece5f8c6b58fb6f20
Sterling De Jesus <s.dejesusp@gmail.com>
2020-07-30 18:30:46 -0400

    Using templates dir

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home_page(request):
+    return render(request, "home_page.html", {})
+
+
+
+def home_page_old(request):
     html_ = '''
     <!doctype html>
     <html lang="en">
