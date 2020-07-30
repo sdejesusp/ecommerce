29e5e23b7b3a764e664c4f00134a47b7ad742a66
Sterling De Jesus <s.dejesusp@gmail.com>
2020-07-30 19:27:43 -0400

    Basic html form

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -3,8 +3,30 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hello world",
+        "content" : "Welcome to the home page"
+    }
+    return render(request, "home_page.html", context)
 
+def about_page(request):
+    context = {
+        "title": "About page",
+        "content" : "Welcome to the about page"
+    }    
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Contact page",
+        "content" : "Welcome to the contact page"
+    }
+    if request.method == 'POST':
+        print(request.POST)
+        print(request.POST.get('fullname'))
+        print(request.POST.get('email'))
+        print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
 
 
 def home_page_old(request):
