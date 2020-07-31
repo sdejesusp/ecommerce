a39626a28c870687af326b24d695c00d8807442b
Sterling De Jesus <s.dejesusp@gmail.com>
2020-07-30 23:16:59 -0400

    Django form example with email validation

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
 
 def home_page(request):
     context = {
@@ -17,15 +18,20 @@ def about_page(request):
     return render(request, "home_page.html", context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contact page",
-        "content" : "Welcome to the contact page"
+        "content" : "Welcome to the contact page",
+        "form": contact_form
     }
-    if request.method == 'POST':
-        print(request.POST)
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
+    # if request.method == 'POST':
+    #     print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
 
 
